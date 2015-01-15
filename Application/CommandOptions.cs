70ce78fac87eea6395147cdbf9fd02f2b4611ab6
Mark Provenzano <kj.provenzano@gmail.com>
2015-01-15 18:12:13 -0500

    Added more functionality

--- Application/CommandOptions.cs
+++ Application/CommandOptions.cs
@@ -20,14 +20,16 @@ namespace Application {
 		/// </summary>
 		/// <param name="args">The command line arguments to parse.</param>
 		public CommandOptions(string[] args) {
-			if(args[1].Contains(SupportedFileTypes[0])) {
-				//TODO: Convert a CSV to PSV.
-			}
-			else if(args[1].Contains(SupportedFileTypes[1])) {
-				//TODO: Handle importing the PSV file type.
-			}
-			else if(args[1] == "-h") {
-				//TODO: Display the help menue.
+			if(args.Length > 1) {
+				if(args[1].Contains(SupportedFileTypes[0])) {
+					//TODO: Convert a CSV to PSV.
+				}
+				else if(args[1].Contains(SupportedFileTypes[1])) {
+					//TODO: Handle importing the PSV file type.
+				}
+				else if(args[1] == "-h") {
+					//TODO: Display the help menue.
+				} 
 			}
 		}
 	}
