6e9920be2a05dea6040e85100b6fd9b83324d977
Britt Mathis <britt.mathis@gmail.com>
2015-01-15 14:14:06 -0500

    Autostyle

--- Application/Menu.cs
+++ Application/Menu.cs
@@ -14,7 +14,7 @@ namespace Application {
 			options.ForEach(option => Console.WriteLine(option));
 			Console.Write("Please enter the number which corresponds to your desired option: ");
 			var choice = Convert.ToInt32(Console.ReadLine());
-			switch(choice) {
+			switch (choice) {
 				case 1:
 					ImportDatabase();
 					break;
@@ -35,12 +35,12 @@ namespace Application {
 			Console.Write("Please specify the location of your .psv file: ");
 			var filename = Console.ReadLine();
 			Table table;
-			using(StreamReader psv = new StreamReader(filename)) {
+			using (StreamReader psv = new StreamReader(filename)) {
 				table = File.Exists(filename) ?
 					ParsePSV.GetTable(psv , filename) :
 					null;
 			}
-			if(table != null) {
+			if (table != null) {
 				Shell.Repl(table);
 			}
 			else {
